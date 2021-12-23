22e8f6d3526722f32500e9bdc3ddce6f3b79edbd
simran <vadhwanisimran@gmail.com>
2021-12-23 09:06:32 +0530

    Timezone Task Done.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment-timezone'
+import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngTimezone';
+  date: any = ""
+  time: any = ""
+  selectedLevel: any
+
+
+  data: any[] = [
+    { id: 0, name: "America/New_York" },
+    { id: 1, name: "America/Los_Angeles"},
+    { id: 2, name: "Europe/London" },
+    { id: 3, name: "Asia/Bankonk" }
+  ];
+
+  selected() {
+    console.log(this.selectedLevel)
+  }
+  newstring(){
+    var newstring=this.date +" "+ this.time
+    return newstring
+  }
+  converttime() {
+    var m = moment.tz(this.newstring(),this.selectedLevel.name);
+     console.log(m.tz("Asia/Kolkata").format());
+     console.log(this.time);
+  }
+    
 }
